c41bfc7c57e4fc1905ddf8234e28a1342a4da3a7
tamashiro-syuta <syuta5825@gmail.com>
2022-12-24 11:36:39 +0900

    ✅ テストを追加

--- test/mailboxes/comments_mailbox_test.rb
+++ test/mailboxes/comments_mailbox_test.rb
@@ -1,11 +1,24 @@
 require "test_helper"
 
 class CommentsMailboxTest < ActionMailbox::TestCase
-  # test "receive mail" do
-  #   receive_inbound_email_from_mail \
-  #     to: '"someone" <someone@example.com>',
-  #     from: '"else" <else@example.com>',
-  #     subject: "Hello world!",
-  #     body: "Hello?"
-  # end
+  test "メール本文が、対応するBoardのコメントとして正しく追加されること" do
+    board = Board.create
+    user = User.create(email: "test@example.com")
+
+    assert_difference -> { board.comments.count } do
+      receive_inbound_email_from_mail \
+        to: "#{board.id}-comment@example.com",
+        from: user.email,
+        subject: "メールタイトル",
+        body: "こんにちは！"
+    end
+    comment = board.comments.last
+    puts "============================"
+    puts "comment"
+    puts "============================"
+    assert_equal user, comment.creator
+    puts "============================"
+    puts "============================"
+    assert_equal "こんにちは！", comment.body
+  end
 end
